fe05c36323853530b6fe0957a8539ffa65648461
repo-arch <repo@criuslife.com>
2026-08-22 11:53:14 +0530

    Add companies router to server

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const cors = require('cors');
 require('dotenv').config();
 
 const filesRouter = require('./routes/files');
+const companiesRouter = require('./routes/companies');   // ADD THIS LINE
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/files', filesRouter);
+app.use('/companies', companiesRouter);                   // ADD THIS LINE
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok', message: 'Drive + SQLite file system is running' });
